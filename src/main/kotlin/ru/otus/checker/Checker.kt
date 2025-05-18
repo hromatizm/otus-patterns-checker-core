a3e9dad1c5d9d5ce978219256c9e168d95a702ca
Михалищин Дмитрий Викторович <hromatizm@gmail.com>
2025-05-18 21:15:13 +0300

    Абстрактный класс и исключение

---
+++ src/main/kotlin/ru/otus/checker/Checker.kt
@@ -0,0 +1,20 @@
+package ru.otus.checker
+
+import kotlin.reflect.full.primaryConstructor
+
+abstract class Checker {
+
+    abstract val code: String
+    abstract val expectedValue: Any?
+
+    var next: Checker? = null
+
+    abstract fun check(args: Map<String, Any>): Boolean
+
+    fun getInstance(expectedValue: Any): Checker {
+        val constructor = this::class.primaryConstructor
+            ?: throw NoArgPrimaryConstructorException(className = this::class.simpleName)
+        return constructor.call(expectedValue)
+    }
+
+}
